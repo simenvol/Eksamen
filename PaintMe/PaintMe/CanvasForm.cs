c06fc574b2052331c5fae6af0d726179cc3ea637
NoobsDeSroobs <magnus_elden@hotmail.com>
2017-12-09 19:37:31 +0100

    Made the baseic canvas and drawing systems. Must generate the drawing controller next.

---
+++ PaintMe/PaintMe/CanvasForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PaintMe
+{
+    class CanvasForm : Form
+    {
+
+        public CanvasForm()
+        {
+            base.KeyPreview = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g;
+
+            g = e.Graphics;
+
+            Pen redPen = new Pen(Color.DarkRed);
+            Pen whitePen = new Pen(Color.White);
+
+
+            g.FillRectangle(new SolidBrush(Color.DarkBlue), new Rectangle(0, 0, 500, 500));
+
+
+            whitePen.Width = 60;
+            g.DrawLine(whitePen, -1, -1, 500, 500);
+            g.DrawLine(whitePen, 0, 500, 500, 0);
+            
+            redPen.Width = 30;
+            g.DrawLine(redPen, -1, -1, 500, 500);
+            g.DrawLine(redPen, 0, 500, 500, 0);
+            g.DrawString("Hello Simen!", new Font("Arial", 24), new SolidBrush(Color.Black), new Point(150, 50));
+        }
+
+    }
+
+
+
+}
---
+++ PaintMe/PaintMe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace PaintMe
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        static void Main(string[] args)
+        {
+            Form f = new CanvasForm();
+            f.BackColor = Color.White;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.SetBounds(500, 500, 500, 500);
+            f.TopMost = true;
+
+
+            
+            Application.EnableVisualStyles();
+            Application.Run(f);
+        }
+
+
+    }
+}
