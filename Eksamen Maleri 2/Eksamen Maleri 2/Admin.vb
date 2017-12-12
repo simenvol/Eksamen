59d616257383e5877e3f463b670f1dcfa4fe2bba
simenvol <simenvol@gmail.com>
2017-12-12 17:38:32 +0100

    Simen

--- Eksamen Maleri 2/Eksamen Maleri 2/Admin.vb
+++ Eksamen Maleri 2/Eksamen Maleri 2/Admin.vb
@@ -20,6 +20,7 @@
         updateincome()
         adminlist()
     End Sub
+    'Magnus er nørd
     Private Sub updateincome() 'calculates total sales by adding up highest bid of paintings where status is sold
         Dim sumsales As String
         Dim newbid As String
